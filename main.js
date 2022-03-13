40ef4e755012c19ea37ef1b702ef7d55939f3617
Suraj <sharmasurajkumar4649@gmail.com>
2022-03-13 14:11:05 +0530

    Add main.js and Create new command 1.help 2.organize and some code add in this

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
-// hello suraj
-function abc()
-{
-    console.log("hello");
-}
+// entry point of my command line
+let helpFun=require("./command/help");
 
-abc();
+let inputArr=process.argv.slic(2);
+let command=inputArr[0];
+let path=inputArr[0];
+
+switch(command)
+{
+    case "tree":
+        // call tree function
+        console.log("tree function called succesfully");
+        break;
+    case "organize":
+        // call organize function
+        console.log("organize function called succesfully");
+        break;
+    case "help":
+        //call help function
+        helpFun.help();
+        break;
+    default:
+        console.log("command not recognized:");
+        break;    
+
+}
